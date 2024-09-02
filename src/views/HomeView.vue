201c480e9e171cb904822098a4cd60c64eca8a1e
AkhonaMj <akhona0304mjikelo@gmail.com>
2024-09-02 14:56:11 +0200

    added front end

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,53 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="left-side">
+      <h1 class="bolder">Bells' Beauty Botique</h1>
+      <p class="nice">Welcome to a home of beauty and glamour!</p>
+    </div>
+    <div class="right-side">
+      <img src="https://via.placeholder.com/300" alt="Beauty and Glamour Image">
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  name: 'HomeView'
 }
 </script>
+
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+}
+
+.left-side {
+  flex-basis: 40%;
+}
+
+.right-side {
+  flex-basis: 60%;
+}
+
+.bold {
+  font-weight: bold;
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+
+.nice {
+  font-size: 18px;
+  color: #666;
+}
+
+.right-side img {
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+  border-radius: 10px;
+}
+</style>
