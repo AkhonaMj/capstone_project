986e3baf9bb74d514d74d91d7b7d859d90fed991
AkhonaMj <akhona0304mjikelo@gmail.com>
2024-09-12 09:31:00 +0200

    -

--- index.js
+++ index.js
@@ -10,24 +10,11 @@ let port = process.env.PORT || 2001
 const app = express()
 app.use(express.static('public'))
 app.use(express.json())
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "*");
-    res.header("Access-Control-Request-Methods", "*");
-    res.header("Access-Control-Allow-Headers", "*");
-    res.header("Access-Control-Expose-Headers", "Authorization");
-    next();
-});
-app.use(cors())
-//{
-  //   origin: "http://localhost:8080",
-  //   credentials:true
-  // }
-
+app.use(cors({
+  origin: "http://localhost:8080",
+  credentials:true
+}))
 app.use(cookieParser()) 
-app.use(express.static('public'))
-app.use(express.json())
 app.use("/users",userRouter)
 app.use("/items",itemRouter)
 app.use("/orders",orderRouter)
