7869c1f4024579d852ab91a8e6396748de887809
AkhonaMj <akhona0304mjikelo@gmail.com>
2024-09-12 09:26:26 +0200

    -

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ import orderRouter from "./routes/orderRoutes.js"
 
 let port = process.env.PORT || 2001
 const app = express()
+app.use(express.static('public'))
+app.use(express.json())
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", "true");
@@ -16,13 +18,13 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Headers", "*");
     res.header("Access-Control-Expose-Headers", "Authorization");
     next();
-  })
-app.use(cors(
-//   {
-//   origin: "http://localhost:8080",
-//   credentials:true
-// }
-))
+});
+app.use(cors())
+//{
+  //   origin: "http://localhost:8080",
+  //   credentials:true
+  // }
+
 app.use(cookieParser()) 
 app.use(express.static('public'))
 app.use(express.json())
