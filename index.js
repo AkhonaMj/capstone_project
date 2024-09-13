38b81b1d232f9b40f0478086a93aa0d3ede6b593
AkhonaMj <akhona0304mjikelo@gmail.com>
2024-09-13 14:02:50 +0200

    cors error

--- index.js
+++ index.js
@@ -9,10 +9,16 @@ let port = process.env.PORT || 2001
 const app = express()
 app.use(express.static('public'))
 app.use(express.json())
-app.use(cors({
-  origin: "https://capstone-50858.firebaseapp.com/",
-  credentials:true
-}))
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Request-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Expose-Headers", "Authorization");
+  next();
+});
+app.use(cors())
 app.use(cookieParser()) 
 app.use("/users",userRouter)
 app.use("/items",itemRouter)
